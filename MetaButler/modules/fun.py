736a5f143aff491c4307979d66b0a9d970cc70ce
destroyer19991 <bpsgusain@gmail.com>
2021-05-29 20:57:41 +0530

    minor fix

--- MetaButler/modules/fun.py
+++ MetaButler/modules/fun.py
@@ -35,7 +35,7 @@ def slap(update: Update, context: CallbackContext):
     user_id = extract_user(message, args)
 
     if user_id == bot.id:
-        temp = random.choice(fun_strings.SLAP_Meta_TEMPLATES)
+        temp = random.choice(fun_strings.SLAP_MetaButler_TEMPLATES)
 
         if isinstance(temp, list):
             if temp[2] == "tmute":
