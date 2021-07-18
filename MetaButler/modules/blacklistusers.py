9bd0ed681a2896ba02706e4b085ecdb13504bcf0
DESTROYER-32 <bpsgusain@gmail.com>
2021-07-18 19:46:06 +0530

    blacklistusers: simplify logic

--- MetaButler/modules/blacklistusers.py
+++ MetaButler/modules/blacklistusers.py
@@ -14,7 +14,7 @@ from MetaButler.modules.helper_funcs.extraction import extract_user, extract_use
 from MetaButler.modules.log_channel import gloggable
 from telegram import ParseMode, Update
 from telegram.error import BadRequest
-from telegram.ext import CallbackContext, CommandHandler, run_async
+from telegram.ext import CallbackContext
 from telegram.utils.helpers import mention_html
 from MetaButler.modules.helper_funcs.decorators import metacmd
 
@@ -47,12 +47,10 @@ def bl_user(update: Update, context: CallbackContext) -> str:
     try:
         target_user = bot.get_chat(user_id)
     except BadRequest as excp:
-        if excp.message == "User not found":
-            message.reply_text("I can't seem to find this user.")
-            return ""
-        else:
+        if excp.message != 'User not found':
             raise
-
+        message.reply_text("I can't seem to find this user.")
+        return ''
     sql.blacklist_user(user_id, reason)
     message.reply_text("I shall ignore the existence of this user!")
     log_message = (
@@ -122,10 +120,7 @@ def bl_users(update: Update, context: CallbackContext):
             users.append(f"• {mention_html(user.id, user.first_name)}")
 
     message = "<b>Blacklisted Users</b>\n"
-    if not users:
-        message += "Noone is being ignored as of yet."
-    else:
-        message += "\n".join(users)
+    message += "\n".join(users) if users else "Noone is being ignored as of yet."
 
     update.effective_message.reply_text(message, parse_mode=ParseMode.HTML)
 
