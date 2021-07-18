c58ee244bc07141f770ed4ad7276659e154fcf72
DESTROYER-32 <bpsgusain@gmail.com>
2021-07-18 19:50:25 +0530

    shout: pull a one-liner

--- MetaButler/modules/shout.py
+++ MetaButler/modules/shout.py
@@ -7,8 +7,7 @@ from MetaButler.modules.helper_funcs.decorators import metacmd
 def shout(update: Update, context: CallbackContext):
     args = context.args
     text = " ".join(args)
-    result = []
-    result.append(" ".join(list(text)))
+    result = [" ".join(list(text))]
     for pos, symbol in enumerate(text[1:]):
         result.append(symbol + " " + "  " * pos + symbol)
     result = list("\n".join(result))
