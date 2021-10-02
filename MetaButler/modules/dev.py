adf037bc936e754af6440c5466696df5301d81de
DESTROYER-32 <bpsgusain@gmail.com>
2021-10-02 14:58:47 +0530

    Dev: Get chat info by ID

--- MetaButler/modules/dev.py
+++ MetaButler/modules/dev.py
@@ -1,10 +1,11 @@
+import html
 import os
 import subprocess
 import sys
 from time import sleep
 from MetaButler import dispatcher, telethn, OWNER_ID, DEV_USERS
 from MetaButler.modules.helper_funcs.chat_status import dev_plus
-from telegram import TelegramError, Update
+from telegram import TelegramError, Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler
 import asyncio
 from statistics import mean
@@ -68,10 +69,39 @@ async def getstats(event):
         f"**__META EVENT STATISTICS__**\n**Average messages:** {messages.average()}/s\n**Average Callback Queries:** {callback_queries.average()}/s\n**Average Inline Queries:** {inline_queries.average()}/s", parse_mode='md'
         )
 
+@dev_plus      
+def get_chat_by_id(update: Update, context: CallbackContext):
+    msg = update.effective_message
+    args = context.args
+    if not args:
+        msg.reply_text("<i>Chat ID required</i>", parse_mode=ParseMode.HTML)
+        return
+    if len(args) >= 1:
+        data = context.bot.get_chat(args[0])
+        m = "<b>Found chat, below are the details.</b>\n\n"
+        m += "<b>Title</b>: {}\n".format(html.escape(data.title))
+        m += "<b>Members</b>: {}\n\n".format(data.get_members_count())
+        if data.description:
+            m += "<i>{}</i>\n\n".format(html.escape(data.description))
+        if data.linked_chat_id:
+            m += "<b>Linked chat</b>: {}\n".format(data.linked_chat_id)
+        
+        m += "<b>Type</b>: {}\n".format(data.type)
+        if data.username:
+            m += "<b>Username</b>: {}\n".format(html.escape(data.username))
+        m += "<b>ID</b>: {}\n".format(data.id)
+        m += "\n<b>Permissions</b>:\n <code>{}</code>\n".format(data.permissions)
+        
+        if data.invite_link:
+            m += "\n<b>Invitelink</b>: {}".format(data.invite_link)
+        
+        msg.reply_text(text=m, parse_mode=ParseMode.HTML)
 
 LEAVE_HANDLER = CommandHandler("leave", leave, run_async=True)
+GET_CHAT_HANDLER = CommandHandler("getchat", get_chat_by_id, run_async=True)
 
 dispatcher.add_handler(LEAVE_HANDLER)
+dispatcher.add_handler(GET_CHAT_HANDLER)
 
 __mod_name__ = "Dev"
-__handlers__ = [LEAVE_HANDLER]
+__handlers__ = [LEAVE_HANDLER, GET_CHAT_HANDLER]
