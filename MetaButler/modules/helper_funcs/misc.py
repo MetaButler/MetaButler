f4d5bb3299507ab978a1388b94568ac605fef54a
DESTROYER-32 <bpsgusain@gmail.com>
2022-01-12 23:04:07 +0530

    Replace Nekobin with PrivateBin

--- MetaButler/modules/helper_funcs/misc.py
+++ MetaButler/modules/helper_funcs/misc.py
@@ -1,8 +1,17 @@
 from typing import Dict, List
+import typing
 from uuid import uuid4
 from MetaButler import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, InlineQueryResultArticle, InputTextMessageContent
 from telegram.error import TelegramError
+import requests
+import json
+import zlib
+import base64
+from urllib.parse import urlparse, urljoin, urlunparse
+import base58
+from Crypto import Random, Hash, Protocol
+from Crypto.Cipher import AES
 from math import ceil
 
 
@@ -30,9 +39,8 @@ def split_message(msg: str) -> List[str]:
         else:
             result.append(small_msg)
             small_msg = line
-    else:
-        # Else statement at the end of the for loop, so append the leftover string.
-        result.append(small_msg)
+    # Else statement at the end of the for loop, so append the leftover string.
+    result.append(small_msg)
 
     return result
 
@@ -52,7 +60,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         )
     else:
             modules = sorted(
-                [
+                 [
                 EqInlineKeyboardButton(
                     x.__mod_name__,
                     callback_data="{}_module({},{})".format(
@@ -103,6 +111,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                 ),
             )
         ]
+
     return pairs
 
 def article(
@@ -155,14 +164,12 @@ def build_keyboard(buttons):
 
 
 def revert_buttons(buttons):
-    res = ""
-    for btn in buttons:
-        if btn.same_line:
-            res += "\n[{}](buttonurl://{}:same)".format(btn.name, btn.url)
-        else:
-            res += "\n[{}](buttonurl://{})".format(btn.name, btn.url)
-
-    return res
+    return "".join(
+        "\n[{}](buttonurl://{}:same)".format(btn.name, btn.url)
+        if btn.same_line
+        else "\n[{}](buttonurl://{})".format(btn.name, btn.url)
+        for btn in buttons
+    )
 
 
 def build_keyboard_parser(bot, chat_id, buttons):
@@ -180,6 +187,22 @@ def build_keyboard_parser(bot, chat_id, buttons):
 
 def is_module_loaded(name):
     return name not in NO_LOAD
+
+def upload_text(data: str) -> typing.Optional[str]:
+    passphrase = Random.get_random_bytes(32)
+    salt = Random.get_random_bytes(8)
+    key = Protocol.KDF.PBKDF2(passphrase, salt, 32, 100000, hmac_hash_module=Hash.SHA256)
+    compress = zlib.compressobj(wbits=-15)
+    paste_blob = compress.compress(json.dumps({'paste': data}, separators=(',', ':')).encode()) + compress.flush()
+    cipher = AES.new(key, AES.MODE_GCM)
+    paste_meta = [[base64.b64encode(cipher.nonce).decode(), base64.b64encode(salt).decode(), 100000, 256, 128, 'aes', 'gcm', 'zlib'], 'syntaxhighlighting', 0, 0]
+    cipher.update(json.dumps(paste_meta, separators=(',', ':')).encode())
+    ct, tag = cipher.encrypt_and_digest(paste_blob)
+    resp = requests.post('https://bin.nixnet.services', headers={'X-Requested-With': 'JSONHttpRequest'}, data=json.dumps({'v': 2, 'adata': paste_meta, 'ct': base64.b64encode(ct + tag).decode(), 'meta': {'expire': '1week'}}, separators=(',', ':')))
+    data = resp.json()
+    url = list(urlparse(urljoin('https://bin.nixnet.services', data['url'])))
+    url[5] = base58.b58encode(passphrase).decode()
+    return urlunparse(url)
 
 def delete(delmsg, timer):
     sleep(timer)
--- MetaButler/modules/paste.py
+++ MetaButler/modules/paste.py
@@ -1,34 +1,54 @@
-import requests
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext
 from MetaButler.modules.helper_funcs.decorators import metacmd
+import privatebinapi
+from io import BytesIO
 
 @metacmd(command='paste', pass_args=True)
 def paste(update: Update, context: CallbackContext):
     args = context.args
     message = update.effective_message
-
+    
     if message.reply_to_message:
-        data = message.reply_to_message.text
-
+        data = message.reply_to_message.text or message.reply_to_message.caption
+        if message.reply_to_message.document:
+            file_info = context.bot.get_file(message.reply_to_message.document.file_id)
+            with BytesIO() as file:
+                file_info.download(out=file)
+                file.seek(0)
+                data = file.read().decode()
+                
     elif len(args) >= 1:
-        data = message.text.split(None, 1)[1]
-
+        data = message.text.split()[1:]
     else:
-        message.reply_text("What am I supposed to do with this?")
+        message.reply_text('What am I supposed to do with this?')
         return
-
-    key = (
-        requests.post("https://nekobin.com/api/documents", json={"content": data})
-        .json()
-        .get("result")
-        .get("key")
-    )
-
-    url = f"https://nekobin.com/{key}"
-
-    reply_text = f"Nekofied to *Nekobin* : {url}"
-
-    message.reply_text(
-        reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
-    )
+    
+    txt = ''
+    pvt_bin_response = privatebinapi.send('https://bin.nixnet.services', text = data, expiration = '1week', formatting = 'syntaxhighlighting')
+    if not pvt_bin_response['full_url']:
+        txt = 'Failed to paste data'
+    else:
+        txt = '<b>Successfully uploaded to PrivateBin:</b> {0}\n<b>Your paste deleting token is:</b> <code>{1}</code>\n<b>Paste Deletion Time: 1 Week</b>'.format(pvt_bin_response["full_url"], pvt_bin_response["deletetoken"])
+    
+    message.reply_text(txt, disable_web_page_preview=True, parse_mode=ParseMode.HTML)
+    
+@metacmd(command='delpaste', pass_args=True)
+def delpaste(update: Update, context: CallbackContext) -> None:
+    args = context.args
+    msg = update.effective_message
+    
+    if len(args) >= 2:
+        full_url = msg.text.split()[1]
+        del_token = msg.text.split()[2]
+    else:
+        msg.reply_text('What am I supposed to do with this?')
+        return
+    
+    delete_response = privatebinapi.delete(full_url, del_token)
+    if delete_response is not None:
+        if int(delete_response['status']) == 0:
+            msg.reply_text('<b>Successfully deleted paste!</b>', parse_mode=ParseMode.HTML)
+            return
+    else:
+        msg.reply_text('<b>Paste does not exist or wrong token provided!</b>', parse_mode=ParseMode.HTML)
