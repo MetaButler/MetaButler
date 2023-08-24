e06b6b6018dc23f192683cfeb655c0b3562369f2
DESTROYER-32 <bpsgusain@gmail.com>
2023-08-24 16:25:38 +0530

    welcomes: use chatmember updates for welcome/goodbye
    Co-authored-by: Luke <me@luuke.ml>

--- MetaButler/modules/helper_funcs/chat_status.py
+++ MetaButler/modules/helper_funcs/chat_status.py
@@ -103,7 +103,7 @@ def is_user_ban_protected(update: Update, user_id: int, member: ChatMember = Non
             or user_id in DEV_USERS
             or user_id in WHITELIST_USERS
             or chat.all_members_are_administrators
-            or (msg.reply_to_message and msg.reply_to_message.sender_chat is not None
+            or (msg and msg.reply_to_message and msg.reply_to_message.sender_chat is not None
                 and msg.reply_to_message.sender_chat.type != "channel")
     ):
         return True
