1e4fb18ab244a774c503725d4b58e28c17745432
DESTROYER-32 <bpsgusain@gmail.com>
2023-10-12 13:10:39 +0530

    Revert "Merge pull request #16 from AnonymousX1025/patch-1"
    This reverts commit 35a193e00136022467a7b19dc69efb0268ee5d91, reversing
    changes made to 1e333848a69273c5aad9ac92eb50bfd65f812a8f.

--- MetaButler/modules/helper_funcs/handlers.py
+++ MetaButler/modules/helper_funcs/handlers.py
@@ -1,8 +1,13 @@
 import telegram.ext as tg
 from telegram import MessageEntity, Update
-from pyrate_limiter import BucketFullException, Duration, Limiter, Rate
-
 from MetaButler import DEV_USERS, SUDO_USERS, WHITELIST_USERS, SUPPORT_USERS
+from pyrate_limiter import (
+    BucketFullException,
+    Duration,
+    RequestRate,
+    Limiter,
+    MemoryListBucket,
+)
 
 try:
     from MetaButler import CUSTOM_CMD
@@ -22,12 +27,17 @@ class AntiSpam:
         )
         # Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
         Duration.CUSTOM = 15  # Custom duration, 15 seconds
-        self.sec_limit = Rate(6, Duration.CUSTOM)  # 6 / Per 15 Seconds
-        self.min_limit = Rate(20, Duration.MINUTE)  # 20 / Per minute
-        self.hour_limit = Rate(100, Duration.HOUR)  # 100 / Per hour
-        self.daily_limit = Rate(1000, Duration.DAY)  # 1000 / Per day
-        self.rates = [self.sec_limit, self.min_limit, self.hour_limit, self.daily_limit]
-        self.limiter = Limiter(self.rates)
+        self.sec_limit = RequestRate(6, Duration.CUSTOM)  # 6 / Per 15 Seconds
+        self.min_limit = RequestRate(20, Duration.MINUTE)  # 20 / Per minute
+        self.hour_limit = RequestRate(100, Duration.HOUR)  # 100 / Per hour
+        self.daily_limit = RequestRate(1000, Duration.DAY)  # 1000 / Per day
+        self.limiter = Limiter(
+            self.sec_limit,
+            self.min_limit,
+            self.hour_limit,
+            self.daily_limit,
+            bucket_class=MemoryListBucket,
+        )
 
     def check_user(self, user):
         """
